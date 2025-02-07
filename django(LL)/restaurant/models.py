a5c65751f7cdec42777ed10710f0db6bbe2e34fa
Wiliam Albano <WilliamAlbano28@gmail.com>
2025-02-07 00:51:02 +0000

    Made some progress and installed the database

--- django(LL)/restaurant/models.py
+++ django(LL)/restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.time}"
