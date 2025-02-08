a2a8bd037d1eedbec381cf3863776b32563ccea5
Wiliam Albano <WilliamAlbano28@gmail.com>
2025-02-08 15:48:02 +0000

    New update, start working with the django rest framework

--- django(LL)/restaurant/views.py
+++ django(LL)/restaurant/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+# Create your views here.  
+from rest_framework import generics  
+from .models import Menu  
+from .serializers import UserSerializer  
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):  
+    queryset = Menu.objects.all()  
+    serializer_class = UserSerializer  
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):  
+    queryset = Menu.objects.all()  
+    serializer_class = UserSerializer  
